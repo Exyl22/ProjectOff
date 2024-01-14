2aad1af5374c7560465587cc15f5ba794803cd7e
exyyyl <exyyyl@DESKTOP-VKFJ27I>
2024-01-14 21:57:17 +0300

    Добавьте файлы проекта.

---
+++ ProjectOff/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ProjectOff
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderRadius = 5;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.OliveDrab;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(23, 243);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(148, 36);
+            this.guna2Button1.TabIndex = 0;
+            this.guna2Button1.Text = "Выключить ПК";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.BorderRadius = 5;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(23, 189);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "Количество секунд до выключения ПК";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(262, 48);
+            this.guna2TextBox1.TabIndex = 1;
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderRadius = 5;
+            this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button2.FillColor = System.Drawing.Color.LightCoral;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.Location = new System.Drawing.Point(177, 243);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.Size = new System.Drawing.Size(108, 36);
+            this.guna2Button2.TabIndex = 2;
+            this.guna2Button2.Text = "Отмена";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(692, 364);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.guna2Button1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProjectOff/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProjectOff
+{
+    public partial class MainForm : Form
+    {
+        private System.Windows.Forms.Timer hideLabelTimer;
+        private CancellationTokenSource shutdownToken;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            hideLabelTimer = new System.Windows.Forms.Timer();
+            hideLabelTimer.Interval = 5000; // 5 секунд
+            hideLabelTimer.Tick += HideLabelTimer_Tick;
+            label1.Visible = false; // Изначально скрываем лейбл
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(guna2TextBox1.Text, out int seconds))
+            {
+                // Очистить текстбокс
+                guna2TextBox1.Clear();
+
+                // Вывести уведомление в MessageBox и лейбле
+                ShowNotification($"Компьютер будет выключен через {seconds} секунд", "Выключение", MessageBoxIcon.Information);
+
+                // Создать токен отмены
+                shutdownToken = new CancellationTokenSource();
+
+                // Начать отсчет
+                Thread countdownThread = new Thread(() =>
+                {
+                    for (int i = seconds; i > 0; i--)
+                    {
+                        // Проверка на отмену
+                        if (shutdownToken.Token.IsCancellationRequested)
+                        {
+                            // Вызов метода для отмены уведомления в лейбле
+                            CancelNotification();
+                            return;
+                        }
+
+                        // Обновление Label с отображением времени
+                        UpdateStatusLabel($"Осталось времени: {i} секунд");
+
+                        Thread.Sleep(1000); // Подождать 1 секунду
+                    }
+
+                    // Выключение компьютера
+                    Process.Start("shutdown", "/s /t 0");
+                });
+                countdownThread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное количество секунд", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            // Проверка на заполнение текстбокса перед отменой
+            if (string.IsNullOrWhiteSpace(guna2TextBox1.Text))
+            {
+                MessageBox.Show("Пожалуйста, заполните текстбокс", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Отменить выключение
+            if (shutdownToken != null)
+            {
+                shutdownToken.Cancel();
+                // Вывести уведомление в MessageBox и лейбле
+                ShowNotification("Выключение отменено", "Отмена", MessageBoxIcon.Information);
+                // Активировать текстбокс после отмены
+                guna2TextBox1.Enabled = true;
+            }
+        }
+
+        // Метод для безопасного обновления элемента управления из другого потока
+        private void UpdateStatusLabel(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => label1.Text = text));
+            }
+            else
+            {
+                label1.Text = text;
+            }
+        }
+
+        // Метод для вывода уведомления в MessageBox и лейбле
+        private void ShowNotification(string message, string caption, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, icon);
+            UpdateStatusLabel(message);
+            label1.Visible = true; // Показать лейбл
+            hideLabelTimer.Start(); // Запустить таймер для автоматического скрытия лейбла
+            // Деактивировать текстбокс во время отсчета
+            guna2TextBox1.Enabled = false;
+        }
+
+        // Метод для отмены уведомления в лейбле
+        private void CancelNotification()
+        {
+            UpdateStatusLabel("Выключение отменено");
+            hideLabelTimer.Start(); // Запустить таймер для автоматического скрытия лейбла
+            // Деактивировать текстбокс после отмены
+            guna2TextBox1.Enabled = false;
+        }
+
+        // Обработчик события таймера для автоматического скрытия лейбла
+        private void HideLabelTimer_Tick(object sender, EventArgs e)
+        {
+            label1.Visible = false; // Скрыть лейбл
+            hideLabelTimer.Stop(); // Остановить таймер
+            // Активировать текстбокс после скрытия лейбла
+            guna2TextBox1.Enabled = true;
+        }
+    }
+}
